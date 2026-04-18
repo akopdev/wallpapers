7db55e3acf7e417a9cf05a226ed5d985b1d27af9
Akop Kesheshyan <hello@akop.dev>
2026-04-18 14:26:05 +0200

    change background every 10 minutes

--- background.sh
+++ background.sh
@@ -2,4 +2,8 @@
 
 # set a random background wallpaper for i3wm
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
-feh --randomize --bg-fill "${SCRIPT_DIR}"/wallpapers/*.{jpg,jpeg,png}
+while true; do
+    feh --randomize --bg-fill "${SCRIPT_DIR}"/wallpapers/*.{jpg,jpeg,png}
+    # Wait for 10 minutes
+    sleep 60*10
+done
